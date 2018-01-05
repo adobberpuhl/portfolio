898788a09e3a73e535485238ad29bc39da673e89
allie <adobberpuhl@gmail.com>
2018-01-04 20:44:06 -0600

    twelfth-commit

--- js/master.js
+++ js/master.js
@@ -1,9 +1,15 @@
-$(document).ready(function() {
-    var $toggleButton = $('.ham'),
-        $menuWrap = $('.nav');
-
-    $toggleButton.on('click', function() {
-        $(this).toggleClass('buttonOpen');
-        $menuWrap.toggleClass('menuShow');
-    });
+$(window).load(function() {
+   $("section").each(function(index) {
+	    $(this).delay(400*index).slideDown(300);
+	});
 });
+
+// $(document).ready(function() {
+//     var $toggleButton = $('.ham'),
+//         $menuWrap = $('.nav');
+
+//     $toggleButton.on('click', function() {
+//         $(this).toggleClass('buttonOpen');
+//         $menuWrap.toggleClass('menuShow');
+//     });
+// });
